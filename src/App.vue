225dec1e18bfead7d6d5486cb6add5d533327983
Gideon Peters <my@pers.on.al>
2021-02-16 13:03:37 +0100

    chore: borders

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div class="title-text px-10">PettySave</div>
+    <router-view />
   </div>
 </template>
 
@@ -15,6 +16,15 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: black;
+  border: 6px solid white;
+}
+
+.title-text {
+  -webkit-text-stroke: 5px black;
+  font-size: 26rem;
+  color: white;
+  opacity: 0.2;
 }
 
 #nav {
